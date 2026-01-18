0ecf3692d378df92ea26807c3531fa662c58f959
22piyush <piyushaglawe05@gmail.com>
2026-01-18 18:55:15 +0530

    start forgot password page

--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../store/slices/authSlice";
-import { BookOpen } from "lucide-react";
+import { BookOpen, Loader } from "lucide-react";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
@@ -94,7 +94,94 @@ const LoginPage = () => {
             <div className="inline-flex items-center justify-center w-16 h-16 bg-blue-500 rounded-full mb-4">
               <BookOpen className="h-8 w-8 text-white" />
             </div>
-            <h1>Educational Project Management</h1>
+            <h1 className="text-2xl font-bold text-slate-800">
+              Educational Project Management
+            </h1>
+            <p className="text-slate-600 mt-2">Sign in to your account</p>
+          </div>
+
+          {/* Login Form  */}
+          <div className="card">
+            <form onSubmit={handleSubmit} className="space-y-6">
+              {errors.general && (                                //1
+                <div className="p-3 bg-red-50 border border-red-200 rounded-lg">
+                  <p className="text-sm text-red-600">{errors.general}</p>
+                </div>
+              )}
+
+              {/* Role Selection  */}
+              <div>
+                <label className="label">Select Role</label>
+                <select
+                  className="input"
+                  name="role"
+                  value={formData.role}
+                  onChange={handleChange}
+                >
+                  <option value="Student">Student</option>
+                  <option value="Teacher">Teacher</option>
+                  <option value="Admin">Admin</option>
+                </select>
+              </div>
+
+              {/* Email Address  */}
+              <div>
+                <label className="label">Email Address</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className={`input ${errors.email ? "input-error" : ""}`}
+                  placeholder="Enter your email"
+                />
+                {
+                  errors.email && (
+                    <p className="text-sm text-red-600 mt-1">{errors.email}</p>
+                  )
+                }
+              </div>
+
+              {/* Password */}
+              <div>
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className={`input ${errors.email ? "input-error" : ""}`}  //2
+                  placeholder="Enter your password"
+                />
+                {
+                  errors.password && (
+                    <p className="text-sm text-red-600 mt-1">{errors.password}</p>
+                  )
+                }
+              </div>
+
+
+              {/* Forgot Password Link */}
+              <div className="text-right">
+                 <Link to={"/forgot-password"} className="text-sm text-blue-600 hover:text-blue-500">
+                  Forgot your password?
+                  </Link>
+              </div>
+
+
+              {/* Submit button  */}
+              <button type="submit" disabled={isLoggingIn} className="w-full btn-primary disabled:opacity-50 disabled:cursor-not-allowed">
+                {
+                  isLoggingIn ? (
+                    <div className="flex justify-center items-center">
+                      <Loader className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"/>
+                      Siging in...
+                    </div>
+                  ) : "Sign In"
+                }
+              </button>
+
+            </form>
           </div>
         </div>
       </div>
