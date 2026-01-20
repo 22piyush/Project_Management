a2c019d1f0b139631efa925c2db56d2bf2db49e4
22piyush <piyushaglawe05@gmail.com>
2026-01-20 21:31:36 +0530

    start backend

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,14 +1,19 @@
 import { asyncHandler } from "../middlewares/asyncHandler.js"
 import ErrorHandler from "../middlewares/error.js";
 import { User } from "../models/user.js";
-
+import * as userServices from "../services/userServices";
 
 export const createStudent = asyncHandler(async (req, res, next) => {
 
-    const {name, email, password, department} = req.body;
-    if(!name || !email || !password || !department){
+    const { name, email, password, department } = req.body;
+    if (!name || !email || !password || !department) {
         return next(new ErrorHandler("Please provide all required fields", 400))
     }
-    const user =  await 
+    const user = await userServices.createUser({ name, email, password, department, role: "Student" });
 
+    res.status(201).json({
+        success: true,
+        message: "Student created successfully",
+        data: { user },
+    });
 })
