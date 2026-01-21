82d30933b05ab30a66b51f314acc7557ae63081e
22piyush <piyushaglawe05@gmail.com>
2026-01-21 21:18:57 +0530

    start backend

--- client/src/store/slices/adminSlice.js
+++ client/src/store/slices/adminSlice.js
@@ -1,7 +1,99 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosInstance } from "../../lib/axios";
 import { toast } from "react-toastify";
+import { act } from "react";
 // import { createDeadline } from "./deadlineSlice";
+
+export const createStudent = createAsyncThunk(
+  "createStudent", 
+  async(data, thunkAPI) => {
+    try{
+      const res = await axiosInstance.post("/admin/create-student", data);
+      toast.success(res.data.message || "Student created successfully");
+      return res.data.data.user;
+    }catch(error){
+      toast.error(error.response?.data?.message || "Failed to create student");
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+});
+
+export const updateStudent = createAsyncThunk(
+  "updateStudent", 
+  async({id, data}, thunkAPI) => {
+    try{
+      const res = await axiosInstance.put(`/admin/update-student/${id}`, data);
+      toast.success(res.data.message || "Student updated successfully");
+      return res.data.data.user;
+    }catch(error){
+      toast.error(error.response?.data?.message || "Failed to update student");
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+});
+
+export const deleteStudent = createAsyncThunk(
+  "deleteStudent", 
+  async({id}, thunkAPI) => {
+    try{
+      const res = await axiosInstance.delete(`/admin/delete-student/${id}`);
+      toast.success(res.data.message || "Student delete successfully");
+      return id;
+    }catch(error){
+      toast.error(error.response?.data?.message || "Failed to delete student");
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+});
+
+
+export const createTeacher = createAsyncThunk(
+  "createTeacher", 
+  async(data, thunkAPI) => {
+    try{
+      const res = await axiosInstance.post("/admin/create-teacher", data);
+      toast.success(res.data.message || "Teacher created successfully");
+      return res.data.data.user;
+    }catch(error){
+      toast.error(error.response?.data?.message || "Failed to create teacher");
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+});
+
+export const updateTeacher = createAsyncThunk(
+  "updateTeacher", 
+  async({id, data}, thunkAPI) => {
+    try{
+      const res = await axiosInstance.put(`/admin/update-teacher/${id}`, data);
+      toast.success(res.data.message || "Teacher updated successfully");
+      return res.data.data.user;
+    }catch(error){
+      toast.error(error.response?.data?.message || "Failed to update teacher");
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+});
+
+export const deleteTeacher = createAsyncThunk(
+  "deleteTeacher", 
+  async({id}, thunkAPI) => {
+    try{
+      const res = await axiosInstance.delete(`/admin/delete-teacher/${id}`);
+      toast.success(res.data.message || "Teacher delete successfully");
+      return id;
+    }catch(error){
+      toast.error(error.response?.data?.message || "Failed to delete Teacher");
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+});
+
+export const getAllUsers = createAsyncThunk(
+  "getAllUsers", 
+  async({id}, thunkAPI) => {
+    try{
+      const res = await axiosInstance.get("/admin/users");
+      return res.data.data;
+    }catch(error){
+      toast.error(error.response?.data?.message || "Failed to fetched users");
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+});
 
 const adminSlice = createSlice({
   name: "admin",
@@ -15,7 +107,27 @@ const adminSlice = createSlice({
     error: null,
   },
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+    .addCase(createStudent.fulfilled, (state, action) => {
+      if(state.users) state.users.unshift(action.payload);
+    })
+    .addCase(updateStudent.fulfilled, (state, action) => {
+      if(state.users){
+        state.users.map((u) => u._id === action.payload._id ? {...u, ...action.payload} : u
+        );
+      } 
+    })
+    .addCase(deleteStudent.fulfilled, (state, action) => {
+      if(state.users) 
+        state.users = state.users.filter((u) => u._id !== action.payload);
+    })
+    .addCase(getAllUsers.fulfilled, (state, action) => {
+      if(state.users) 
+        state.users = action.payload.users;
+    })
+
+  },
 });
 
 export default adminSlice.reducer;
