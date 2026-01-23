a8f78547911dd50f7d839a62a20896ead0692769
22piyush <piyushaglawe05@gmail.com>
2026-01-24 00:42:00 +0530

    done

--- server/models/project.js
+++ server/models/project.js
@@ -9,5 +9,69 @@ const projectSchema = new mongoose.Schema({
     supervisor: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-    }
-})
+        default: null,
+    },
+    title: {
+        type: String,
+        required: [true, "Project title is required"],
+        trim: true,
+        maxlength: [200, "Title cannot be more than 200 charecter"]
+    },
+    description: {
+        type: String,
+        required: [true, "Project description is required"],
+        trim: true,
+        maxlength: [2000 , "Description cannot be more than 2000 charecter"]
+    },
+    status: {
+        type: String,
+        default: "pending",
+        enum: ["pending", "approved", "rejected", "completed"],
+    },
+    files: [
+        {
+            fileType:{
+                type: String,
+                required: true,
+            },
+            fileUrl:{
+                type: String,
+                required: true,
+            },
+            originalName:{
+                type: String,
+                required: true,
+            },
+            uploadedAt:{
+                type: String,
+                default: Date.now,
+            },
+                           
+        }
+    ],
+    feedback: [
+        {
+            supervisorId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+                required: true,
+            },
+            type: {
+                type: String,
+                enum: ["positive", "negative", "general"],
+                default: "general",
+            },
+            title: {
+                type: String,
+                required: true,
+            },
+            message: {
+                type: String,
+                required: true,
+                maxlength: [2000 , "Feedback cannot be more than 1000 charecter"]
+            }
+        }
+    ]
+},{
+    timestamps: true,
+});
