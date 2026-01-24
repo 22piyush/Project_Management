13fef9a0e1ed1b87ba50eaa86193c73a3655ecb1
22piyush <piyushaglawe05@gmail.com>
2026-01-24 23:31:47 +0530

    done

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -4,23 +4,56 @@ import { User } from "../models/user.js";
 import * as userServices from "../services/userServices.js";
 import * as projectServices from "../services/projectServices";
 
-
 export const getStudentProject = asyncHandler(async (req, res, next) => {
-
     const studentId = req.user._id;
 
     const project = await projectServices.getProjectByStudent(studentId);
 
-    if(!project){
+    if (!project) {
         return res.status(200).json({
             success: true,
             data: { project: null },
-            message:"No project found for this student"
+            message: "No project found for this student",
         });
     }
     return res.status(200).json({
         success: true,
         data: { project },
     });
+});
 
-})
+export const submitProposal = asyncHandler(async (req, res, next) => {
+    const { title, description } = req.body;
+    const studentId = req.user._id;
+
+    const existingProject = await projectServices.getProjectByStudent(studentId);
+
+    if (existingProject && existingProject.status !== "rejected") {
+        return next(
+            new ErrorHandler(
+                "You already have an active project. Ypu can only submit new proposal if previous project is rejected.",
+                400,
+            ),
+        );
+    }
+
+    const projectData = {
+        student: studentId,
+        title,
+        description
+    };
+
+    const project = await projectServices.createProject(projectData);
+
+    await User.findByIdAndUpdate(studentId, { project: project._id });
+
+    res.status(200).json({
+        success: true,
+        data: { project },
+        message: "Project proposal submitted successfully."
+    });
+    
+});
+
+
+export const uploadFiles
--- server/services/projectServices.js
+++ server/services/projectServices.js
@@ -4,3 +4,8 @@ import  { Project } from "../models/project"
 export const getProjectByStudent = async (studentId) => {
     return await Project.findOne({student: studentId}).sort({ createdAt: -1 });
 }
+
+
+export const createProject = async(projectData) => {
+    
+}
